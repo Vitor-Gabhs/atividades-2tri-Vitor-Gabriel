364611734ac9335bcb6aed3d76da64d05292df97
Vitor-Gabhs <vitor.dutra.marques@escola.pr.gov.br>
2026-07-03 16:34:06 -0300

    atv 15 feita

--- atividades_Back/atividade15.js
+++ atividades_Back/atividade15.js
@@ -8,17 +8,26 @@ app.listen(3000, () => {
 });
 
 let tarefa = [
-    { id: 1, titulo: "Café com Deus Pai"},
-    { id: 2, titulo: "Reunião de Pais"},
-    { id: 3, titulo: "Aula de Matemática"},
-    { id: 4, titulo: "Aula de História"}
+    { id: 1, titulo: "Ler Café com Deus Pai"},
+    { id: 2, titulo: "Ir á Reunião de Pais"},
+    { id: 3, titulo: "Frequentar a Aula de Matemática"},
+    { id: 4, titulo: "Frequentar a Aula de História"}
 ];
 
-app.get('/tarefas', (req, res) => {
+app.get('/tarefa', (req, res) => {
     res.json(tarefa);
 });
 
-app.post('/tarefas', (req, res) => {
+app.get('/tarefa/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const tarefaEncontrada = tarefa.find(item => item.id === id);
+    if (!tarefaEncontrada) {
+        return res.status(404).json({ mensagem: 'tarefa não encontrada' });
+    };
+    res.json(tarefaEncontrada);
+});
+
+app.post('/tarefa', (req, res) => {
     const novaTarefa = {
         id: tarefa.length + 1,
         titulo: req.body.titulo
@@ -27,7 +36,72 @@ app.post('/tarefas', (req, res) => {
     res.status(201).json(novaTarefa);
 });
 
-app.put('/tarefas/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const tarefaIndex = tarefa.find(item => item.id === id);  
+app.put('/tarefa/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const tarefaEncontrada = tarefa.find(item => item.id === id);  
 
+    if (!tarefaEncontrada) {
+        return res.status(404).json({ mensagem: 'Tarefa não encontrada' });
+    };
+
+tarefaEncontrada.titulo = req.body.titulo;
+res.json(tarefaEncontrada);
+});
+
+app.delete('/tarefa/:id', (req, res) => {
+    const id = Number(req.params.id);
+    tarefa = tarefa.filter(item => item.id !== id);
+    res.json({mensagem: "tarefa removida."});
+});
+
+//------------------------------------------------------------------------------
+
+let usuario = [
+    { id: 1, nome: "Matheus", email: "matheus@email.com"},
+    { id: 2, nome: "Renam", email: "renam@email.com"},
+    { id: 3, nome: "Maria", email: "maria@email.com"},
+    { id: 4, nome: "João", email: "joao@email.com"}
+];
+
+app.get('/usuario', (req, res) => {
+    res.json(usuario);
+});
+
+app.get('/usuario/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const usuarioEncontrado = usuario.find(item => item.id === id);
+    if (!usuarioEncontrado) {
+        return res.status(404).json({ mensagem: 'Usuário não encontrado' });
+    };
+    res.json(usuarioEncontrado);
+});
+
+app.post('/usuario', (req, res) => {
+    const novoUsuario = {
+        id: usuario.length + 1,
+        nome: req.body.nome,
+        email: req.body.email
+    };
+    usuario.push(novoUsuario);
+    res.status(201).json(novoUsuario);
+});
+
+app.put('/usuario/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const usuarioEncontrado = usuario.find(item => item.id === id);  
+
+    if (!usuarioEncontrado) {
+        return res.status(404).json({ mensagem: 'Usuário não encontrado' });
+    };
+
+usuarioEncontrado.nome = req.body.nome;
+usuarioEncontrado.email = req.body.email;
+
+res.json(usuarioEncontrado);
+});
+
+app.delete('/usuario/:id', (req, res) => {
+    const id = Number(req.params.id);
+    usuario = usuario.filter(item => item.id !== id);
+    res.json({mensagem: "Usuário removido."});
+});
