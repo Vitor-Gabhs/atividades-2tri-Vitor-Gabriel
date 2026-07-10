7f5d1baf14c62458d50a7254108ddb9db15c7d80
vitor-gabhs <vitor.dutra.marques@escola.pr.gob.br>
2026-07-09 22:18:18 -0300

    falta 17, 18

---
+++ atividades_Front/atividade9.js
@@ -0,0 +1,16 @@
+/* 1.
+{
+    "Titulo": "Férias de meio de ano",
+    "Corpo": "pois é! Elas não são delírio coletivo..."
+}
+_________________________________________
+ 2.
+{
+    "nome": "João Gabriel
+    "email": "joao70correr@email.com"
+    "perfil": "aluno"
+}
+_________________________________________
+ 3.
+
+*/ 
