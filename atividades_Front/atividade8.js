7a57ceb4e946e138eabc26d623b188f5757adaae
Vitor-Gabhs <vitor.dutra.marques@escola.pr.gov.br>
2026-07-10 19:25:06 -0300

    recolocando...

--- atividades_Front/atividade8.js
+++ atividades_Front/atividade8.js
@@ -1,14 +1,16 @@
-// 1. Solicitou buscar por algum nome dentro de um mock / Buscar as roupas compradas por um usuario
-// 2. Adicionou uma nova roupa para um historico do cliente / adicionou um novo usuario
-// 3. quando o servidor não encontra um endpoint/pagina
-// 4. endpoint foi criada com sucesso
-// 5. POST /avisos HTTP/1.1
-//    Host: localhost:3000
-//    Content-Type: application/json
-//    
-//    "Novo aviso: depois de amanhã é ferias!"
-//__________________________________
-//    HTTP/1.1 201 Created
-//    Content-Type: application/json
-//    
-//    "Novo aviso: depois de amanhã é ferias!"
+/* 
+ 1. Solicitou buscar por algum nome dentro de um mock / Buscar as roupas compradas por um usuario
+ 2. Adicionou uma nova roupa para um historico do cliente / adicionou um novo usuario
+ 3. quando o servidor não encontra um endpoint/pagina
+ 4. endpoint foi criada com sucesso
+ 5. POST /avisos HTTP/1.1
+    Host: localhost:3000
+    Content-Type: application/json
+    
+    "Novo aviso: depois de amanhã é ferias!"
+__________________________________
+    HTTP/1.1 201 Created
+    Content-Type: application/json
+    
+    "Novo aviso: depois de amanhã é ferias!"
+*/
